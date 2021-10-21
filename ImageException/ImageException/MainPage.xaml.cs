2fe9cbfdaf974dd812851bf55e2b1c677ee9de87
vigneshramesh <vignesh.ramesh@syncfusion.com>
2021-10-21 22:41:51 +0530

    Sample included.

---
+++ ImageException/ImageException/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageException
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var assembly = typeof(MainPage).Assembly;
+            string path = "ImageException";
+
+            var stream = assembly.GetManifestResourceStream($"{path}.Image.Image.png");
+            image.Source = ImageSource.FromStream(() => stream);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MainGrid.Children.Remove(image);
+            MainGrid.Children.Add(image);
+        }
+    }
+}
